5c5c2f67624b3d1f86c39fde1e97fe3883d8a2b5
Alejandro Perez Pujante <perezpujantealejandro1@gmail.com>
2022-01-25 17:43:56 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import WelcomeCard from "./components/WelcomeCard.vue";
+import Todos from "./components/Todos.vue";
+</script>
+
+<template>
+  <header>
+    <WelcomeCard />
+  </header>
+  <main class="grid gap-y-3 place-content-center">
+    <Todos />
+  </main>
+</template>
---
+++ src/components/WelcomeCard.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="font-mono dark:text-stone-200 text-2xl">Vue 3 Todo App</h1>
+</template>
